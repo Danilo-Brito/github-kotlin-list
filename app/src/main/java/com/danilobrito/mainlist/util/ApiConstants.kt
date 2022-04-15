e28e78a99c618eff3c2a76778806daceca8474b0
Danilo Brito <danilomelo.brito19@gmail.com>
2022-04-15 13:06:36 -0300

    Api constants

---
+++ app/src/main/java/com/danilobrito/mainlist/util/ApiConstants.kt
@@ -0,0 +1,11 @@
+package com.danilobrito.mainlist.util
+
+class ApiConstants {
+
+    companion object {
+        const val BASE_URL = "https://api.github.com/"
+
+        /*Endpoints*/
+        const val SEARCH_ENDPOINT = "search/repositories"
+    }
+}
