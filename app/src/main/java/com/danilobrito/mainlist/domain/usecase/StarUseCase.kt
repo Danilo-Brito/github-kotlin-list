76ccb77613e76c12daa8a0442bb6e7d864e5b546
Danilo Brito <danilomelo.brito19@gmail.com>
2022-04-16 18:07:55 -0300

    Deleting unecessary files

--- app/src/main/java/com/danilobrito/mainlist/domain/usecase/StarUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.danilobrito.mainlist.domain.usecase
-
-import com.danilobrito.mainlist.data.repository.GithubRepository
-
-class StarUseCase(
-    private val repository: GithubRepository
-) {
-    suspend operator fun invoke() = repository.repositoryByStar()
-}
